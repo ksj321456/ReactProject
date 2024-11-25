37fb7df430c0f747efc8d8464624a31c3ace2aca
ksj321456 <ksj321456@gmail.com>
2024-11-25 15:47:36 +0900

    최종코드

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './LoginPage';
-import HomePage from './HomePage';
-<<<<<<< HEAD
-import RegisterPage from './RegisterPage';
-import Portfolio from './Portfolio';
-import CoinTable from './CoinTable';
-import CoinChart from './CoinChart';
-=======
->>>>>>> origin/master
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./LoginPage";
+import HomePage from "./HomePage";
+import RegisterPage from "./RegisterPage";
+import Portfolio from "./Portfolio";
+import CoinTable from "./CoinTable";
+import CoinChart from "./CoinChart";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage />} />
-<<<<<<< HEAD
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/main" element={<CoinTable />} />
-                <Route path="/portfolio" element={<Portfolio/>}/>
-                <Route path='/chart' element={<CoinChart />} />
-=======
->>>>>>> origin/master
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/main" element={<CoinTable />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/chart" element={<CoinChart />} />
+      </Routes>
+    </Router>
+  );
 }
 
-<<<<<<< HEAD
 export default App;
-=======
-export default App;
->>>>>>> origin/master
