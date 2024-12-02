ae1258372d60f3a2022af3d19db22291aa0d71fb
ksj321456 <ksj321456@gmail.com>
2024-12-02 13:42:42 +0900

    최종

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useNavigate } from "react-router-dom"; // useHistory 추가
 import React from "react";
 function HomePage() {
@@ -12,43 +11,18 @@ function HomePage() {
   };
 
   return (
-    <div>
-      <h1>가상화폐 모의투자시스템</h1>
-      <button type="button" className="login" onClick={handleLogin}>
-        로그인
-      </button>
-      <button type="button" className="register" onClick={handleRegister}>
-        회원가입
-      </button>
+    <div className="container">
+      <div className="overlay">
+        <h1>가상화폐 모의투자시스템</h1>
+        <button type="button" className="home" onClick={handleLogin}>
+          로그인
+        </button>
+        <button type="button" className="home" onClick={handleRegister}>
+          회원가입
+        </button>
+      </div>
     </div>
   );
 }
 
 export default HomePage;
-=======
-import { useNavigate } from 'react-router-dom'; // useHistory 추가
-import React from 'react';
-function HomePage() {
-    const navigate = useNavigate();
-    const handleLogin = () => {
-        navigate('/login'); // 로그인으로 이동
-    };
-
-    const handleRegister = () => {
-        navigate('/register'); // 회원가입으로 이동
-    };
-    
-    return (
-        <div className="container">
-            <div className="overlay">
-            <h1>가상화폐 모의투자시스템</h1>
-            <button type="button" className="home" onClick={handleLogin}>로그인</button>
-            <button type="button" className="home" onClick={handleRegister}>회원가입</button>
-        </div>
-        </div>
-        
-    );
-}
-
-export default HomePage;
->>>>>>> endd
